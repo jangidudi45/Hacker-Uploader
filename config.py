39d380d088506b6234098bc2361a47a19de0dcc5
veer708 <veerahirwal51@gmail.com>
2025-05-19 08:10:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-api_id =  28829496
-api_hash  = '8094341dbe5bd50090de8b12a01d04b8'
-bot_token =  '8121411999:AAHj-WZRHzzXrVFcohrAHuKrqVZ85E3GYqk'
-LOG =  -100253268913
+api_id =  22216834
+api_hash  = 'c50044544e93eb670fbe5ad8d952fe7f'
+bot_token =  ''
+LOG =  -1002268980506
 
