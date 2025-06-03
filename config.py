f867e7b62f6865775f618812d43af551075a3285
jangidudi45 <jangidudit45@gmail.com>
2025-06-03 12:45:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-api_id =  29232353
-api_hash  = '6868788228291767c90e4346eea03f36'
+api_id =  23163380
+api_hash  = '2dca155e786c7db2d295e5b4ab10783b'
 bot_token =  ''
-LOG =  -1002562868665
+LOG =  -1002502504562
 
